f86134466aa74d35b0bec54b5601ab64efe570be
azizeesvg <azizee.svg@gmail.com>
2024-02-18 16:23:20 +0700

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis earum temporibus assumenda eos obcaecati eveniet officiis aliquam eligendi magni impedit facilis ipsum unde ipsa enim, quia sit ullam. Vitae, id.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima aut nobis sit, quasi explicabo aliquam sapiente beatae nisi. Deserunt dolor architecto laudantium accusantium saepe illum praesentium laborum, dolore similique vero in explicabo, eaque rerum iure incidunt. Nostrum sequi, possimus deserunt nemo mollitia perspiciatis veniam, enim delectus reiciendis, totam dolore? Ea.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
